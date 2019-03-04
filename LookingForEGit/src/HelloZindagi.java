207cd5180bb9ab83eb5c96a5c9f1b78834509143
Easwari Venkatramani <eshuprad@gmail.com>
2019-03-04 11:50:25 +0000

    learning GIT
    displays a Hello message and adds 2 numbers supplied via command line argument.

---
+++ LookingForEGit/src/HelloZindagi.java
@@ -0,0 +1,10 @@
+
+public class HelloZindagi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Zindagi");
+		System.out.println("The sum of the two numbers="+Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+	}
+
+}
